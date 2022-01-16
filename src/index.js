84257b6ecf6b82849441ebc84071f0726f800b9b
Taviking <wiking1317@gmail.com>
2022-01-16 11:05:18 +0200

    Added accordion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import modal from "./modules/modal.js";
 import anchor from "./modules/anchor.js";
+import accordeon from "./modules/accordeon.js";
 
+accordeon();
 anchor(".top-menu>ul");
 modal();
